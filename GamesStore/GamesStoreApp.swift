5cc0f929dd0da027e74e9d60a9fbf8f05951467e
jawahendy <50572163+jawahendy@users.noreply.github.com>
2024-01-18 10:55:45 +0700

    Initial Commit

---
+++ GamesStore/GamesStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamesStoreApp.swift
+//  GamesStore
+//
+//  Created by Hendy Nurfrianto on 18/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GamesStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
